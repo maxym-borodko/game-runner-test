163defcbf4521dd2b1ec522cd9d77abcf15d1dbd
Maxym Borodko <max.borodko@gmail.com>
2020-06-17 13:44:46 +0300

    added jump functionality

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    //
     [SerializeField] float speed = 0.5f;
     [SerializeField] int numberOfLanes = 3;
     [SerializeField] int currentLane = 2;
     [SerializeField] float laneWidth = 3;
+    [SerializeField] float jumpSpeed = 7;
+
+    //
+    Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
@@ -32,12 +42,18 @@ public class PlayerControl : MonoBehaviour
             currentLane++;
         }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.velocity += Vector3.up * jumpSpeed;
+        }
+
+
         currentLane = Mathf.Clamp(currentLane, 1, numberOfLanes);
 
         //
         Vector3 newPosition = transform.position;
         newPosition.x = laneWidth * (currentLane - 2);
 
-        transform.position = newPosition;
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, 0.5f);//newPosition;
     }
 }
