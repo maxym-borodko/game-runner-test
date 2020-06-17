7d67b1665842615697ae2b9b04eef49b1b640720
Maxym Borodko <max.borodko@gmail.com>
2020-06-17 12:25:06 +0300

    PlayerControl script added

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] float speed = 0.5f;
+    [SerializeField] int numberOfLanes = 3;
+    [SerializeField] int currentLane = 2;
+    [SerializeField] float laneWidth = 3;
+
+    void Update()
+    {
+        Move();
+        CheckInput();
+    }
+
+    private void Move()
+    {
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentLane--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            currentLane++;
+        }
+
+        currentLane = Mathf.Clamp(currentLane, 1, numberOfLanes);
+
+        Debug.Log(currentLane);
+
+        //
+        Vector3 newPosition = transform.position;
+        newPosition.x = laneWidth * (currentLane - 2);
+
+        transform.position = newPosition;
+    }
+}
